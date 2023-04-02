011404f717769b42047fa8d781ff58891ed870f0
= <=>
2023-04-02 22:37:36 +0100

    clean up

--- prune_files/__init__.py
+++
@@ -1,3 +0,0 @@
-# import sys
-# print('Shame - prune_files dir __init__.py')
-# print(sys.argv)
--- walker.py
+++
@@ -1,9 +0,0 @@
-import os
-from os import PathLike
-from pathlib import Path
-from typing import Callable, Union
-
-
-def walk(directory: Union[str, PathLike], cb: Callable):
-  for current_path, dirs, files in os.walk(Path(directory), topdown=True):
-    cb(files, dirs, current_path)
